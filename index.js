5393be44e46d1628ac9a15d13681036144309fc4
rungkung <rungnan008@gmail.com>
2017-11-24 15:42:07 +0700

    test

--- index.js
+++ index.js
@@ -10,3 +10,9 @@
 https://hapijs.com/
 
 */
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+server.connection({port:3008, host:'localhost'});
+server.start(()=>{
+    console.log('server start');
+});
