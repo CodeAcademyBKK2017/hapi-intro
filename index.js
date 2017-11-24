c464fe71c1fd4c2e78b4651fa296fff4bd4ed6a5
rungkung <rungnan008@gmail.com>
2017-11-24 15:55:03 +0700

    test

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ https://hapijs.com/
 const Hapi = require('hapi');
 const server = new Hapi.Server();
 server.connection({port:3008, host:'localhost'});
+server.route({
+    method: 'GET',
+    path:'/login',
+    handler:(request,reply)=>{
+        console.log(request.query);
+        reply('Hello from HapiJS');
+    }
+})
 server.start(()=>{
     console.log('server start');
 });
