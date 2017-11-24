167809ab6800be4c792d0f03545d105788d28f00
Jack <jakkrit.chin@gmail.com>
2017-11-24 15:53:29 +0700

    add route

--- index.js
+++ index.js
@@ -15,7 +15,15 @@ const Hapi = require('hapi');
 
 const server = new Hapi.Server();
 
-server.connection({port: 3008, host: 'localhost'});
+server.connection({ port: 3008, host: 'localhost' });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        return reply('Hello from HapiJS');
+    }
+});
 
 server.start(() => {
     console.log('Server started. Yay!');
