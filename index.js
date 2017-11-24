803c11e78e44580e19464fa78b7be0fd5e8bd33d
Teeraphong Chaichalermpreecha <teeraphong.m@gmail.com>
2017-11-24 15:55:22 +0700

    hello

--- index.js
+++ index.js
@@ -17,6 +17,16 @@ const server = new Hapi.Server();
 
 server.connection({port: 3000, host: 'localhost'});
 
+server.route({
+  method: 'GET',
+  path: '/login',
+  handler: (request, reply) => {
+    // console.log('request.params', request.params);
+    // console.log('request.query', request.query);
+    reply('Hello from HapiJS');
+  }
+});
+
 server.start(() => {
-  console.log('Server start');
+  console.log('Server start !');
 });
