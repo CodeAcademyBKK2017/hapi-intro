803c54c14dbd6fbb06bce85b1fdc96840740748d
Deathsky <wbenjapol@gmail.com>
2017-11-24 15:41:45 +0700

    DSADSADAS

--- index.js
+++ index.js
@@ -10,3 +10,14 @@
 https://hapijs.com/
 
 */
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+
+server.connection({port: 3008,host: 'localhost'});
+
+server.start(() => {
+//   if (err) {
+//     throw err;
+//   }
+  console.log(`Server running at: ${server.info.uri}`);
+});
