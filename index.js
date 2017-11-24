2a825bbb5ab740ebad5c214e20368bd347cc9738
Chanchai <chanchailim@gmail.com>
2017-11-24 15:54:20 +0700

    Hello

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ const server = new Hapi.Server();
 
 server.connection({ port: 3000, host: 'localhost' });
 
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, reply) => {
+        // console.log('request:', request.query);
+        reply('Hello from HAPIJS')
+    }
+});
+
 server.start(() => {
     console.log('Server started. Yay!');
 });
