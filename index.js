e2b0dbd489d5304bb32a7a546349423e4d38bf71
Deathsky <wbenjapol@gmail.com>
2017-11-24 15:53:54 +0700

    DSADSADAS

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ const Hapi = require('hapi');
 const server = new Hapi.Server();
 
 server.connection({port: 3008,host: 'localhost'});
+server.route({
+  method: 'GET',
+  path: '/login',
+  handler: (request,reply) => {
+    console.log('request',request.query);
+    reply('Hello From HapiJS');
+  }
+});
 
 server.start(() => {
 //   if (err) {
