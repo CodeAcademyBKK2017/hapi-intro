1735ef8f25d85135ae6e931c00eaa71da09b183a
Jack <jakkrit.chin@gmail.com>
2017-11-27 11:01:53 +0700

    update code

--- handlers/user-data.js
+++ handlers/user-data.js
@@ -5,14 +5,11 @@ module.exports = (request, reply) => {
   const userQuery = request.query.user || '';
   const userData = info.data.find((user) => user.name.toLowerCase() === userQuery.toLowerCase());
 
-  let result = '';
-
   if (userData) {
-    result = reply(fs.readFileSync(`./assets/data/${userData.dataFile}`, 'utf8'));
-  } else {
-    result = 'User not found.';
+    fs.readFile(`./assets/data/${userData.dataFile}`, 'utf8', (err, data) => {
+      reply(data);
+    });
   }
 
-
-  return reply(result);
+  return reply('User not found.');
 };
